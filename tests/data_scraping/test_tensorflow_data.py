d8c23702980a7fead78e1459acf48429b15e2751
bamblebam <burhanizrangwala@gmail.com>
2021-07-26 09:09:15 +0530

    bug module import issue

---
+++ tests/data_scraping/test_tensorflow_data.py
@@ -0,0 +1,10 @@
+from unittest.mock import patch
+import data_scraping.tensorflow_data as tf_data
+
+# @patch('data_scraping.tensorflow_data.get_data_from_repository')
+# def test_get_data_from_repository(mock_get_data_from_repository):
+#     #function to test wether the function get_data_from_repository is working properly
+#     #uses the patch decorator to mock the function get_data_from_repository
+#     #checks if the sequential_list is filled properly
+#     expected=mock_get_data_from_repository('url','driver','startTime','path')
+#     assert expected
