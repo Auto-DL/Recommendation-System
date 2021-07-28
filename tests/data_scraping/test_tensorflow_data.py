1a8ade0ae18ac992f7eef863824636722d24f758
bamblebam <burhanizrangwala@gmail.com>
2021-07-28 08:36:56 +0530

    unit testing

--- tests/data_scraping/test_tensorflow_data.py
+++ tests/data_scraping/test_tensorflow_data.py
@@ -1,5 +1,22 @@
+import os
+from pathlib import Path
 from unittest.mock import patch
+import pytest
 import data_scraping.tensorflow_data as tf_data
+
+
+@pytest.fixture
+def get_test_code():
+    path = Path(os.getcwd()) / "tests" / "mock_resources" / "test_code.txt"
+    with open(path, "r") as f:
+        content = f.read()
+        f.close()
+    return content
+
+
+@pytest.fixture
+def get_test_model():
+    return ["Conv2D", "Maxpooling2D", "Flatten", "Dense", "Dense"]
 
 
 @patch("data_scraping.tensorflow_data.get_data_from_repository")
@@ -9,3 +26,13 @@ def test_get_data_from_repository(mock_get_data_from_repository):
     # checks if the sequential_list is filled properly
     expected = mock_get_data_from_repository("url", "driver", "startTime", "path")
     assert expected
+
+
+def test_model_to_pickle(tmpdir, get_test_model):
+    tf_data.model_to_pickle(get_test_model, tmpdir)
+    assert len(os.listdir(tmpdir)) == 1
+
+
+def test_get_layer_sequence1_helper(get_test_code):
+    expected = tf_data.getLayerSequence1helper(get_test_code)
+    assert len(expected) == 2
